be55eca8d162edb02e25e593cc0acf47abcf0d43
Joe Taylor <iamjoetaylor@gmail.com>
2022-09-13 20:34:52 -0700

    esp32s3 adafruit-example

--- blockware/adafruit-example/src/adafruit_benchmark.h
+++ blockware/adafruit-example/src/adafruit_benchmark.h
@@ -1,5 +1,6 @@
 /*************************************************** 
   Taken from Adafruit's SSD1351 example
  ****************************************************/
+#include <Adafruit_SSD1351.h>
 
-void runAdafruitBenchmark();
+void runAdafruitBenchmark(Adafruit_SSD1351 *tft);
--- blockware/adafruit-example/src/main.cpp
+++ blockware/adafruit-example/src/main.cpp
@@ -3,18 +3,20 @@
 #include "adafruit_benchmark.h"
 #include <DefaultConfig.h>
 
-Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
+Adafruit_SSD1351 *tft;
 
 void setup(void) {
   Serial.begin(SERIAL_DATA_RATE);
   Serial.printf("Starting adafruit-example with SPI speed %d", SPI_SPEED);
-  tft.begin(SPI_SPEED);
-}
+  SPIClass *spi = new SPIClass();
+  spi->begin(SCLK_PIN, -1, MOSI_PIN, CS_PIN);
+  tft = new Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, spi, CS_PIN, DC_PIN, RST_PIN);
+  tft->begin(SPI_SPEED);}
 
 void loop() {
   uint16_t start = millis();
 
-  runAdafruitBenchmark();
+  runAdafruitBenchmark(tft);
   
   uint16_t time = millis() - start;
   Serial.printf("done in %dms\n", time);
